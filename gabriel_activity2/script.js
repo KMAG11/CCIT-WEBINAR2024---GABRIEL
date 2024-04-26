dcadd0961943e0cf0919bc530d535d5faf0fca9d
KMAG11 <122517284+KMAG11@users.noreply.github.com>
2024-04-26 15:31:46 +0800

    Add files via upload

--- gabriel_activity2/script.js
+++ gabriel_activity2/script.js
@@ -1,7 +1,7 @@
 
 const value = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
 const len = 16;
-function generatePassword() {
+function genPass() {
     let password = "";
     for (let i = 0; i < len; i++) {
       const randomNum = Math.floor(Math.random() * value.length);
@@ -12,17 +12,19 @@ function generatePassword() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = generatePassword();
-    document.getElementById('password').value = password;
+    const email = document.querySelector('#email').value;
+    const password = genPass();
+    document.querySelector('#password').value = password;
   }
-  document.getElementById('frmLogin').addEventListener('submit', handleSubmit);
+  document.querySelector('#frmLogin').addEventListener('submit', handleSubmit);
 
   document.querySelector('#button-addon2').addEventListener('click', function() {
-      const passwordInput = document.getElementById('password');
+      const passwordInput = document.querySelector('#password');
       if (passwordInput.type === 'password') {
           passwordInput.type = 'text';
       } else {
           passwordInput.type = 'password';
       }
   });
+
+  // SUBMITTED BY KIARA GABRIEL
